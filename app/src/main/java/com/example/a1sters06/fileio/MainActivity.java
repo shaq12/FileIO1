1c675d547244338627ecd429a6bc6264cc75e8aa
Shaquille Stern <Shaquille Stern>
2017-03-16 11:18:48 +0000

    Initial commit

---
+++ app/src/main/java/com/example/a1sters06/fileio/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.a1sters06.fileio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.save)
+        {
+            //react to the menu item being selected...
+            return true;
+        }
+        return false;
+    }
+
+
+
+}
